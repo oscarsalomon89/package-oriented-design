2d2418c69309c77c35b53892e06cff89893ec14e
maxi <moure.lm@gmail.com>
2023-12-01 06:49:26 -0300

    hola

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,18 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/teamcubation/pod/cmd/api/app"
 	"github.com/teamcubation/pod/internal/platform/environment"
 )
 
 func main() {
 	env := environment.GetFromString(os.Getenv("GO_ENVIRONMENT"))
+	if env == environment.Development {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("error loading env file", err)
+		}
+	}
 
 	dependencies, err := app.BuildDependencies(env)
 	if err != nil {
